02c440af15fb963b8e5963d22d8e0cfbc5235e94
chinhung <chinhung101@gmail.com>
2017-04-23 16:22:42 +0800

    first commit sprong boot first commit

---
+++ src/idv/chinhung/springboot/Hello.java
@@ -0,0 +1,21 @@
+package idv.chinhung.springboot;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class Hello {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+    	System.out.println("request in");
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Hello.class, args);
+    }
+}
