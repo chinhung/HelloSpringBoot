401a8ff8bac587444da14b12cfd26c7d07e8ed23
chinhung <chinhung101@gmail.com>
2017-07-16 22:21:47 +0800

    change basic-auth to simple token-auth

--- src/main/java/idv/chinhung/springboot/SecurityConfig.java
+++ src/main/java/idv/chinhung/springboot/SecurityConfig.java
@@ -1,12 +1,11 @@
 package idv.chinhung.springboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @ComponentScan
@@ -15,12 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().hasAnyRole("USER").and().httpBasic();
-	}
-	
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		  auth.inMemoryAuthentication().withUser("admin1").password("1").roles("ADMIN");
-		  auth.inMemoryAuthentication().withUser("user1").password("1").roles("USER");
+		http.authorizeRequests().anyRequest().permitAll();
+		http.addFilterBefore(new TokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 }
---
+++ src/main/java/idv/chinhung/springboot/TokenFilter.java
@@ -0,0 +1,39 @@
+package idv.chinhung.springboot;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+public class TokenFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		String token = httpRequest.getHeader("TOKEN");
+		if (!isValid(token)) {
+			httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+
+	private boolean isValid(String token) {
+		if (token == null) {
+			return false;
+		}
+		if ("123456".equals(token)) {
+			return true;
+		}
+		return false;
+	}
+
+}
