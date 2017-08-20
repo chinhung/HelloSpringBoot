c48421bbb9a0f23b094c4f1e2e9e4ca1ce928ccf
chinhung <chinhung101@gmail.com>
2017-08-20 18:24:08 +0800

    add WebMvcTest

---
+++ src/test/java/idv/chinhung/springboot/HelloTest.java
@@ -0,0 +1,39 @@
+package idv.chinhung.springboot;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.inject.Inject;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloTest {
+	
+	@Inject
+	private WebApplicationContext webContext;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setupMockMvc() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webContext)
+			.apply(springSecurity()).build();
+	}
+	
+	@Test
+	public void rootTest() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk());
+	}
+}
